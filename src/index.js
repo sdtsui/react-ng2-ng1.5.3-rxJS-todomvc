f37562b882f40bf810d012e257f06aca0bc59488
sdtsui <szehungdanieltsui@gmail.com>
2016-03-30 20:57:36 -0700

    [react] working local, add comment to index

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 var Rx = require('rx');
 var Model = require('./model');
-var React = require('react');
-var ReactDOM = require('react-dom');
-var TodoApp = require('./react/TodoApp');
 import Intent from './intent';
 
-// **since we're not filtering, we should just be able to use Model.subject, as it inherits Observable
 var Observable = Model.subject.map(function (appState) {
   // Do Nothing for Now:
   return appState;
 });
 
+
+// ***** React Implementation *****
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TodoApp = require('./react/TodoApp');
+
 //react entrypoint
 Observable.subscribe((appState) => {
-  //TODO: clean up
   appState.Intent = Intent;
-
   ReactDOM.render(
     <TodoApp {...appState}/>,
     document.getElementById('app')
   );
 });
+
+
+// ***** Angular 1.5.3 Implementation *****
+
+
+
+
+// ***** Angular 2 Implementation *****
