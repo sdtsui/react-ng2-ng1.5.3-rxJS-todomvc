a371a39ef52029f461c588820edd4f5f2cfee5b3
sdtsui <szehungdanieltsui@gmail.com>
2016-04-10 00:41:56 -0700

    [wip ng2] spike : setup redux store

---
+++ ng2-ts-quickstart/src/main.js
@@ -0,0 +1,47 @@
+System.register(['angular2/platform/browser', './components/App.component'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var browser_1, App_component_1;
+    return {
+        setters:[
+            function (browser_1_1) {
+                browser_1 = browser_1_1;
+            },
+            function (App_component_1_1) {
+                App_component_1 = App_component_1_1;
+            }],
+        execute: function() {
+            browser_1.bootstrap(App_component_1.AppComponent);
+        }
+    }
+});
+/**
+ *
+ */
+/**
+ *
+ */
+// console.log('createStore', createStore);
+// import todoApp from '../reducers/index'
+// let store = createStore(todoApp)
+// console.log('new store :', store);
+// 
+// 
+// Configure store like this :
+// import { enableProdMode, provide } from 'angular2/core';
+// import { bootstrap} from 'angular2/bootstrap';
+// import { ROUTER_PROVIDERS, APP_BASE_HREF } from 'angular2/router';
+// import configureStore from './store/configure-store';
+// import { RioSampleApp } from './containers/sample-app';
+// const provider = require('ng2-redux').provider;
+// const store = configureStore({});
+// declare let __PRODUCTION__: any;
+// if (__PRODUCTION__) {
+//   enableProdMode();
+// }
+// bootstrap(RioSampleApp, [
+//   provider(store),
+//   ROUTER_PROVIDERS,
+//   provide(APP_BASE_HREF, { useValue: '/' })
+// ]);
+//# sourceMappingURL=main.js.map
---
+++ ng2-ts-quickstart/src/main.ts
@@ -0,0 +1,38 @@
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './components/App.component';
+bootstrap(AppComponent);
+
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+
+// console.log('createStore', createStore);
+// import todoApp from '../reducers/index'
+// let store = createStore(todoApp)
+// console.log('new store :', store);
+// 
+// 
+// Configure store like this :
+// import { enableProdMode, provide } from 'angular2/core';
+// import { bootstrap} from 'angular2/bootstrap';
+// import { ROUTER_PROVIDERS, APP_BASE_HREF } from 'angular2/router';
+// import configureStore from './store/configure-store';
+// import { RioSampleApp } from './containers/sample-app';
+
+// const provider = require('ng2-redux').provider;
+// const store = configureStore({});
+// declare let __PRODUCTION__: any;
+
+// if (__PRODUCTION__) {
+//   enableProdMode();
+// }
+
+// bootstrap(RioSampleApp, [
+//   provider(store),
+//   ROUTER_PROVIDERS,
+//   provide(APP_BASE_HREF, { useValue: '/' })
+// ]);
